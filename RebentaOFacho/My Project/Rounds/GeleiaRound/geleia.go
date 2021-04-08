a6d3cfb2f40e2f1551018dc69f696650b6222243
Luis Pedro Pinto <luis.pinto@glookast.com>
2021-04-08 13:19:06 +0100

    Improve geleia level

--- RebentaOFacho/My Project/Rounds/GeleiaRound/geleia.go
+++ RebentaOFacho/My Project/Rounds/GeleiaRound/geleia.go
@@ -1,7 +1,7 @@
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/Rounds/GeleiaRound/Geleia_facho/Geleia.atlas\"\n"
+  data: "tile_set: \"/Rounds/GeleiaRound/Geleia.atlas\"\n"
   "default_animation: \"geleia1\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
